0aa628ef0c307bae7251dba34dea7f339368ebd4
takaki <takaki55730317@gmail.com>
2024-04-28 21:04:15 +0900

    Learning Laravel MVC

--- src/app/Http/Controllers/TestController.php
+++ src/app/Http/Controllers/TestController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Test;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    //
+    public function index()
+    {
+        $values = Test::all();
+        // dd($values);
+
+        return view('tests.test', compact('values'));
+    }
 }
