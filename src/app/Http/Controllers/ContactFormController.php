e65c9739c44c767e7803241c9604099fd10acc95
takaki <takaki55730317@gmail.com>
2024-05-19 13:37:45 +0900

    105. Store Requestクラス

--- src/app/Http/Controllers/ContactFormController.php
+++ src/app/Http/Controllers/ContactFormController.php
@@ -33,9 +33,9 @@ class ContactFormController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, ContactForm $contactForm)
     {
-        //
+        dd($request, $request->name);
     }
 
     /**
