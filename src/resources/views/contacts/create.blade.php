d0d5da9e6a8cf3cd02b9eb9f83a3c45d7b61e24d
takaki <takaki55730317@gmail.com>
2024-05-30 18:46:26 +0900

    113. サービスへの切り離し(ファットコントローラ防止) 114. バリデーション(フォームリクエスト)

--- src/resources/views/contacts/create.blade.php
+++ src/resources/views/contacts/create.blade.php
@@ -9,6 +9,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
+                    <x-input-error :messages="$errors->all()" class="mb-4" />
+
                     <section class="text-gray-600 body-font relative">
                         <form method="POST" action="{{ route('contacts.store') }}">
                             @csrf
